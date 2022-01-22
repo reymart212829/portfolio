d6b33083dea88ca6cd4ba5a016c2a5beeb91e429
Reymart Samson <reymart28@gmail.com>
2022-01-23 04:17:29 +0800

    Updates

---
+++ precache-manifest.7ce7f0aae31fda0e5719cfcc4fd1e0ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9eb8453efb65942fdf8f89c24e6d757b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "997a843d01d6f97541c5",
+    "url": "/portfolio/static/css/2.9497e657.chunk.css"
+  },
+  {
+    "revision": "3a4b835a93433e27e5b3",
+    "url": "/portfolio/static/css/main.a7c51c85.chunk.css"
+  },
+  {
+    "revision": "997a843d01d6f97541c5",
+    "url": "/portfolio/static/js/2.a7a4990d.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/portfolio/static/js/2.a7a4990d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a4b835a93433e27e5b3",
+    "url": "/portfolio/static/js/main.85a15a0e.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.42892173fa53b6b360cf508b467eae07.js"
+  "/portfolio/precache-manifest.7ce7f0aae31fda0e5719cfcc4fd1e0ca.js"
 );
 
 self.addEventListener('message', (event) => {
