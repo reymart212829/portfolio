7fda8760e85f3061b84cc3d67c1f203b8dbf57d0
Reymart Samson <reymart28@gmail.com>
2022-01-23 02:14:28 +0800

    Updates

---
+++ precache-manifest.42892173fa53b6b360cf508b467eae07.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "003cadb01f902ce84a83742a9e7f84a8",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "969683c90013aba3aeda",
+    "url": "/portfolio/static/css/2.9497e657.chunk.css"
+  },
+  {
+    "revision": "dc3be11a7dc76c3022f4",
+    "url": "/portfolio/static/css/main.c2c2f9fc.chunk.css"
+  },
+  {
+    "revision": "969683c90013aba3aeda",
+    "url": "/portfolio/static/js/2.06218c59.chunk.js"
+  },
+  {
+    "revision": "efc0bc89bcf702a3aa1127d8ad15fc2e",
+    "url": "/portfolio/static/js/2.06218c59.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dc3be11a7dc76c3022f4",
+    "url": "/portfolio/static/js/main.c158589c.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  }
+]);
